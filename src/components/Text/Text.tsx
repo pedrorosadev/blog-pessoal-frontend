ece6dcf19d26d5144aad46a825beccd33bc5a874
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-11-22 16:49:39 -0300

    Testes Iniciais no REACT

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Text.css";
+
+function Text() {
+  return (
+    <div>
+      <p>Teste de texto de parágrafo</p>
+    </div>
+  );
+}
+
+export { Text };
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Title.css';
+
+interface ITitle{
+    nome:string
+}
+
+function Title(props:ITitle) {
+  return <h1>Hello {props.nome}</h1>;
+}
+
+export { Title };
