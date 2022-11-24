66da132e8a5be8c205d13f1b6634e78f301364dc
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-11-24 12:04:13 -0300

    feat: Adicionando estrutura de Grid no Projeto

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+import {Grid} from "@material-ui/core"
+
+function Home(){
+    return(
+        <>
+        <Grid container>
+            <Grid item>
+
+            </Grid>
+
+            <Grid item container>
+
+            </Grid>
+        </Grid>
+        </>
+    );
+}
