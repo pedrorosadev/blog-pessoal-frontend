27a5da1ac107244e44c4d89a2efa0fcd7994be73
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-11-25 12:10:38 -0300

    feat: Gerenciamento de Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import { Title } from "./components/Title/Title";
-import { Text } from "./components/Text/Text";
+import { Home } from "./pages/home/Home";
+import { About } from "./pages/about/About";
+import { Navbar } from "./components/statics/navbar/Navbar";
 
 function App() {
   const nome = "Pedro";
   return (
-    <>
-      <Title nome={nome} />
-      <Text />
-    </>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import "./Home.css";
-import {Grid} from "@material-ui/core"
+import {Grid} from "@material-ui/core";
 
 function Home(){
     return(
         <>
-        <Grid container>
-            <Grid item>
-
-            </Grid>
-
-            <Grid item container>
-
-            </Grid>
-        </Grid>
+            
         </>
     );
 }
+
+export { Home };
