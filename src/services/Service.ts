b3544b66e59d66ea3b16e2bbe232d99a7fb2d0c7
pedrorosadev <pedrorosadev20@gmail.com>
2022-12-11 11:11:48 -0300

    update: Service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://revolucao-saude.onrender.com'
+    baseURL: 'https://blogpessoal-za2w.onrender.com'
 })
 
     export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
